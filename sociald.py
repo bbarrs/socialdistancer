43a062503d763441057d9885ba30f36d194b8d24
rhinot <rhinospit@hotmail.com>
2020-05-01 05:00:20 +0000

    Setting up primary binary

---
+++ sociald.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""A script for beeping at an increasing rate, dependent on whether another person is at the desired social distance from you.
+
+Hardware:
+- Raspberry Pi Zero
+- Seeed Base Hat for RPi Zero
+- Seeed Ultrasonic Ranger
+- Seeed Buzzer
+"""
+
+__author__ = "@bbarrs, @rhinot"
+__status__ = "Prototype"
+
